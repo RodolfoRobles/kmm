2cbcbc81873f949d70a9b35ffc0344b04d2e0beb
Rodolfo Robles <rm.rodolfo@outlook.com>
2025-05-04 21:22:48 -0400

    Multi app, Android, iOS and gretting text

--- androidApp/src/main/java/com/roblesmoreno/multi/android/MainActivity.kt
+++ androidApp/src/main/java/com/roblesmoreno/multi/android/MainActivity.kt
@@ -3,12 +3,29 @@ package com.roblesmoreno.multi.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.roblesmoreno.multi.Greeting
+import com.roblesmoreno.multi.android.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +36,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    DemoScreen()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun DemoScreen() {
+    var sliderPosition by remember { mutableStateOf(20f) }
+    var handlePositionChange = { position: Float -> sliderPosition = position }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        DemoText(message = Greeting().greet(), fontSize = sliderPosition)
+        Spacer(modifier = Modifier.height(150.dp))
+        DemoSlider(sliderPosition = sliderPosition, onPositionChange = handlePositionChange)
+        Text(text = sliderPosition.toInt().toString(), style = MaterialTheme.typography.bodyLarge)
+    }
 }
 
-@Preview
+@Composable
+fun DemoText(message: String, fontSize: Float) {
+    Text(text = message, fontSize = fontSize.sp, fontWeight = FontWeight.Bold)
+}
+
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..40f,
+        value = sliderPosition,
+        onValueChange = { onPositionChange(it) }
+    )
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
+    LearningComposeTheme {
+        DemoScreen()
     }
 }
